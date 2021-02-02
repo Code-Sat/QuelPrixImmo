c4329f342db9ab971add9eb89f89966cde2163d8
Code-Sat <peiofouillot@hotmail.fr>
2021-02-02 12:02:17 +0100

    Added Graph activity

---
+++ ProjetAndroid/app/src/main/java/peio/fouillot/projetandroid/controller/GraphActivity.java
@@ -0,0 +1,15 @@
+package peio.fouillot.projetandroid.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import peio.fouillot.projetandroid.R;
+
+import android.os.Bundle;
+
+public class GraphActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+    }
+}
